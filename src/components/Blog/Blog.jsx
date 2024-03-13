ac93ee4bdd6121acdfa85d9f5a8ea9b25c05ac20
Anik98B <anikbhowmik0188@gmail.com>
2024-03-14 00:43:08 +0600

    added reading time and bookmark

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
+import { BsBookmarks } from 'react-icons/bs';
 
-const Blog = ({ blog }) => {
-    const { title, cover, author, author_img,posted_date, reading_time,hashtag } = blog;
+const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
+    const { title, cover, author, author_img, posted_date, reading_time, hashtag } = blog;
     return (
-        <div>
-            <img src={cover} alt={`Cover picture of the title ${title}`} />
-            <div className='flex justify-between'>
+        <div className='mb-20 space-y-4'>
+            <img className='w-full mb-8' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between mb-4'>
                 <div className='flex'>
                     <img className='w-14' src={author_img} alt="" />
                     <div className='ml-6'>
@@ -15,20 +16,29 @@ const Blog = ({ blog }) => {
                 </div>
                 <div>
                     <span>{reading_time} min read</span>
+                    <button
+                        onClick={() => handleAddToBookmark(blog)}
+                        className='ml-2 text-2xl text-red-600'><BsBookmarks></BsBookmarks></button>
                 </div>
             </div>
             <h2 className='text-4xl'>{title}</h2>
             <p>
                 {
-                    hashtag.map((hash, idx)=><span key={idx}> <a href="">#{hash}</a></span>)
+                    hashtag.map((hash, idx) => <span key={idx}> <a href="">#{hash}</a></span>)
                 }
             </p>
+            <button
+                onClick={()=>handleMarkAsRead(reading_time)}
+                className='text-purple-800 font-bold underline'>
+                Mark as Read</button>
         </div>
     );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func,
+    handleMarkAsRead: PropTypes.func
 }
 
 export default Blog;
