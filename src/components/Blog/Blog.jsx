da267be5622d1a0e72c6164de5387011f1a0d942
Anik98B <anikbhowmik0188@gmail.com>
2024-03-14 18:40:04 +0600

    finish the project

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { BsBookmarks } from 'react-icons/bs';
 
 const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
-    const { title, cover, author, author_img, posted_date, reading_time, hashtag } = blog;
+    const {id, title, cover, author, author_img, posted_date, reading_time, hashtag } = blog;
     return (
         <div className='mb-20 space-y-4'>
             <img className='w-full mb-8' src={cover} alt={`Cover picture of the title ${title}`} />
@@ -28,7 +28,7 @@ const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
                 }
             </p>
             <button
-                onClick={()=>handleMarkAsRead(reading_time)}
+                onClick={()=>handleMarkAsRead(id,reading_time)}
                 className='text-purple-800 font-bold underline'>
                 Mark as Read</button>
         </div>
